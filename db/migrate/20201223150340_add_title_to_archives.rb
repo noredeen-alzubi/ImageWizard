c90d6168a83481b3a0b936f3634c82342d206ff5
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-12-26 17:27:16 +0200

    Remove more bad migrations.

--- db/migrate/20201223150340_add_title_to_archives.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToArchives < ActiveRecord::Migration[6.0]
-  def change
-    add_column :archives, :title, :string, null: false
-  end
-end
--- db/migrate/20201224163539_drop_archives_table.rb
+++
@@ -1,5 +0,0 @@
-class DropArchivesTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :archives
-  end
-end
